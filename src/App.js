bd2a23a39f4e4161135740e8af461ea4a70018a3
YassG4mer <yassinekrika2016@gmail.com>
2023-02-24 22:41:16 +0100

    final

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Die from './components/Die';
 import { nanoid } from 'nanoid';
@@ -7,22 +7,47 @@ import { nanoid } from 'nanoid';
 function App() {
 
   const [dice, setDice] = useState(allNewDice())
+  const [tenzies, setTenzies] = useState(false)
  
+  useEffect(() => {
+    const allHeld = dice.every(die => die.isHeld)
+    const firstVlaue = dice[0].value
+    const allSameValue = dice.every(die => die.value === firstVlaue)
+
+    if (allHeld && allSameValue) {
+      setTenzies(true)
+      console.log('you won!')
+    }
+
+  }, [dice])
 
   function allNewDice() {
     let array = []
     for (let i = 0; i < 10; i++) {
-      array.push({
-        id: nanoid(), 
-        value: Math.floor(Math.random() * 6), 
-        isHeld: false
-      })
+      array.push(generateNewDie())
     }
     return array
   }
 
   function rollDice() {
-    setDice(allNewDice())
+    if (!tenzies) {
+      setDice(oldDice => oldDice.map(die => {
+        return die.isHeld ?
+                die :
+                generateNewDie()
+      }))
+    } else {
+      setTenzies(false)
+      setDice(allNewDice())
+    }
+  }
+
+  function generateNewDie() {
+    return {
+      id: nanoid(), 
+      value: Math.floor(Math.random() * 6), 
+      isHeld: false
+    }
   }
 
   function holdDice(id) {
@@ -41,10 +66,13 @@ function App() {
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. 
+      Click each die to freeze it at its current value between rolls.</p>
       <div className='dice-container'>
         {diceElements}
       </div>
-      <button onClick={rollDice} className='roll-dice'>Roll</button>
+      <button onClick={rollDice} className='roll-dice'>{tenzies ? "New Game" : "Roll"}</button>
     </main>
   )
 }
