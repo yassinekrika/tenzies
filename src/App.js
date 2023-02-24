4f79d5d18ef15d0c1a21fe74210b8b4a6d82e4e8
YassG4mer <yassinekrika2016@gmail.com>
2023-02-24 19:24:10 +0100

    clickable die

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
 
+import { useState } from 'react';
 import './App.css';
 import Die from './components/Die';
+import { nanoid } from 'nanoid';
 
 function App() {
 
-  let array = [1, 2, 3, 4, 5, 6]
+  const [dice, setDice] = useState(allNewDice())
+ 
+
+  function allNewDice() {
+    let array = []
+    for (let i = 0; i < 10; i++) {
+      array.push({
+        id: nanoid(), 
+        value: Math.floor(Math.random() * 6), 
+        isHeld: false
+      })
+    }
+    return array
+  }
+
+  function rollDice() {
+    setDice(allNewDice())
+  }
+
+  function holdDice(id) {
+    setDice(oldDice => oldDice.map(die => {
+      return die.id === id ? 
+        {...die, isHeld: !dice.isHeld} :
+        die
+    }))
+  }
+
+  const diceElements = dice.map(die => {
+    return <Die key={die.id} value={die.value} isHeld = {die.isHeld} holdDice={()=>holdDice(die.id)} />
+  })
+
+ 
+
   return (
     <main>
       <div className='dice-container'>
-        {array.map(arr => {
-          <Die value={arr}/>
-        })}
+        {diceElements}
       </div>
+      <button onClick={rollDice} className='roll-dice'>Roll</button>
     </main>
   )
 }
--- src/components/Die.js
+++ src/components/Die.js
@@ -1,8 +1,8 @@
 
-export default function Die(Props) {
+export default function Die(props) {
     return (
-        <div className="die-face">
-            <h1 className="die-num">{Props.value}</h1>
+        <div className="die-face" onClick={props.holdDice} style={{backgroundColor: props.isHeld ? '#59e391' : 'white'}} >
+            <h1 className="die-num">{props.value}</h1>
         </div>
     )
 }
