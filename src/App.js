fd97cd6d84eabe98cd1906fbbd9749858dd323b3
YassG4mer <yassinekrika2016@gmail.com>
2023-02-25 11:48:10 +0100

    final Confetti commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import Die from './components/Die';
 import { nanoid } from 'nanoid';
+import Confetti from 'react-confetti'
 
 function App() {
 
@@ -66,6 +67,7 @@ function App() {
 
   return (
     <main>
+      {tenzies && <Confetti />}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">Roll until all dice are the same. 
       Click each die to freeze it at its current value between rolls.</p>
